81c7c7538d1fc8be47461e9cd1413026d8573e5a
lincheng77 <1498047434@qq.com>
2024-12-11 19:26:19 +0800

    1310-转置卷积

--- course/utils/read_utils.py
+++ course/utils/read_utils.py
@@ -6,7 +6,7 @@ import torchvision
 
 def read_data_bananas(is_train=True):
     """读取香蕉检测数据集中的图像和标签"""
-    csv_data = pd.read_csv('./data/banana-detection/bananas_train/label.csv')
+    csv_data = pd.read_csv('../data/banana-detection/bananas_train/label.csv')
 
     # 设置索引列
     csv_data = csv_data.set_index('img_name')
@@ -14,7 +14,7 @@ def read_data_bananas(is_train=True):
     images, targets = [], []
     for img_name, target in csv_data.iterrows():
         # 所有图像
-        images.append(torchvision.io.read_image(os.path.join('./data/banana-detection/bananas_train/images', img_name)))
+        images.append(torchvision.io.read_image(os.path.join('../data/banana-detection/bananas_train/images', img_name)))
 
         # 这里的target包含（类别，左上角x，左上角y，右下角x，右下角y），
         # 其中所有图像都具有相同的香蕉类（索引为0）
